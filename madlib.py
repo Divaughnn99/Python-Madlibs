f42c95fde16580332143de1a1d8872005d68b558
Divaughnn99 <46171705+Divaughnn99@users.noreply.github.com>
2019-01-15 17:56:05 -0800

    Create madlib.py

---
+++ madlib.py
@@ -0,0 +1,16 @@
+
+
+print("THE MIKIAN")
+print("Once apon a time the")
+print("_________(adjective)")
+p1 = input()
+print("Mikian rode a")
+print("_________(adjective)")
+p2 = input()
+print("_________(animal)")
+p3 = input()
+print("and tought people")
+print("_________(a coding language).")
+p4 = input()
+
+print("Once apon a time the", p1, "Mikian rode a", p2, p3, "and tought people", p4 + ".")
